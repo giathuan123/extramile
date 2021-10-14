fa008d2a9611244ed89ca77e8eb4de2f9a6de7bd
Jacob <jacobsfo@gmail.com>
2021-10-13 19:30:04 -0700

    accidents

--- server/data/accidents.js
+++ server/data/accidents.js
@@ -2,7 +2,7 @@ var testData = require("./testData.json");
 var a = JSON.parse(JSON.stringify(testData[0].ID));
 console.log(a);
 
-
+  
 
 //if start and end time = 2016-02-08 this return testdata.id
 Object.keys(testData).forEach(function(key) {
