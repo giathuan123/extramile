72777afb73b570d6432016724de406ab5ea07927
Ivan Carrillo <helloivancar@gmail.com>
2021-10-18 13:02:01 -0700

    Added modal container

---
+++ extramile-react/src/ModalContainer/Modal.js
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom';
+import FocusTrap from 'focus-trap-react'
+
+export const Modal = ({
+    onClickOutside,
+    onKeyDown,
+    modalRef,
+    buttonRef,
+    closeModal,
+    modalBody
+}) => {
+    return ReactDOM.createPortal(
+        <FocusTrap>
+            <aside
+                tabIndex="-1"
+                className="modal-blur"
+                onClick={onClickOutside}
+                onKeyDown={onKeyDown}
+            >
+                <div className="modal-container" ref={modalRef}>
+                    <button
+                        ref={buttonRef}
+                        className="modal-close-button"
+                        onClick={closeModal}
+                    >
+                        <span>
+                            Close
+                        </span>
+                    </button>
+                    
+                    <div className="modal-body">
+                        {modalBody}
+                    </div>
+                </div>
+            </aside>
+        </FocusTrap>,
+    document.body
+    );
+}
--- extramile-react/src/Pages/Search.js
+++ extramile-react/src/Pages/Search.js
@@ -1,10 +1,14 @@
-import {SearchForm} from '../Forms';
+import {CreateRecordForm, SearchForm} from '../Forms';
 import Card from '../components/Card';
 import React from 'react';
+import ModalContainer from '../ModalContainer'
 
 function Search() {
     return (
         <React.Fragment>
+            <ModalContainer triggerText={'Add a Record'}>
+                <CreateRecordForm/>
+            </ModalContainer>
             <SearchForm />
             <Card />
         </React.Fragment>
