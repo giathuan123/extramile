687737f040970da37750c8e1228f6ed36ff1cb50
Estela Ramirez <ramirezestela01@gmail.com>
2021-10-24 22:28:03 -0700

    Added frontend map for feature 1

--- extramile-react/src/Charts/Map.js
+++ extramile-react/src/Charts/Map.js
@@ -1,9 +1,49 @@
-function Map() {
-    return(
-        <>
-        hi this is a map
-        </>
-    );
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+
+class Map extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [{
+                type: 'choropleth',
+                locationmode: 'USA-states',
+                // locations: unpack(rows, 'Postal'),
+                // z: unpack(rows, 'Population'),
+                // text: unpack(rows, 'State'),
+                autocolorscale: true
+            }],
+            layout: {
+                title: '2014 US Popultaion by State',
+                geo:{
+                    scope: 'usa',
+                    countrycolor: 'rgb(255, 255, 255)',
+                    showland: true,
+                    landcolor: 'rgb(217, 217, 217)',
+                    showlakes: true,
+                    lakecolor: 'rgb(255, 255, 255)',
+                    subunitcolor: 'rgb(255, 255, 255)',
+                    lonaxis: {},
+                    lataxis: {}
+                }
+            }
+        };
+    }
+
+    
+    render() {
+        return (
+            <div style={{ width: "100%", height: "100%" }}>
+                
+                <Plot
+                    data={this.state.data}
+                    layout={this.state.layout}
+                />
+            </div>
+        );
+    }
 }
 
 export default Map;
+
--- extramile-react/src/Pages/Maps.js
+++ extramile-react/src/Pages/Maps.js
@@ -3,9 +3,11 @@ import { MapChart } from '../Charts';
 function Maps() {
     return (
         <div>
+            <h1>Number of Accidents Per U.S. State</h1>
             <MapChart />
         </div>
     );
 }
 
 export default Maps;
+
