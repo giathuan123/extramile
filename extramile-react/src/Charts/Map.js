770e45ba32318abb2b6330cc14854f9b0f053e30
Estela Ramirez <ramirezestela01@gmail.com>
2021-10-25 04:41:46 -0700

    changed class component to function

--- extramile-react/src/Charts/Map.js
+++ extramile-react/src/Charts/Map.js
@@ -1,49 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react'
 import Plot from 'react-plotly.js';
 
+function Map() {
+    var locs = []
+    var accidents = []
+    const [list, setList] = useState([]);
+    const [checker,setChecker] = useState(0);
 
-class Map extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [{
-                type: 'choropleth',
-                locationmode: 'USA-states',
-                // locations: unpack(rows, 'Postal'),
-                // z: unpack(rows, 'Population'),
-                // text: unpack(rows, 'State'),
-                autocolorscale: true
-            }],
-            layout: {
-                title: '2014 US Popultaion by State',
-                geo:{
-                    scope: 'usa',
-                    countrycolor: 'rgb(255, 255, 255)',
-                    showland: true,
-                    landcolor: 'rgb(217, 217, 217)',
-                    showlakes: true,
-                    lakecolor: 'rgb(255, 255, 255)',
-                    subunitcolor: 'rgb(255, 255, 255)',
-                    lonaxis: {},
-                    lataxis: {}
-                }
-            }
-        };
-    }
 
-    
-    render() {
-        return (
-            <div style={{ width: "100%", height: "100%" }}>
-                
-                <Plot
-                    data={this.state.data}
-                    layout={this.state.layout}
-                />
-            </div>
-        );
-    }
+    return (
+        <div style={{ width: "100%", height: "100%" }}>
+            <Plot
+                data= {[{
+                    type: 'choropleth',
+                    locationmode: 'USA-states',
+                    locations: locs,
+                    z: accidents, 
+                    //text: ["California", "Ohio"],
+                    autocolorscale: true
+                }]}
+                layout= { {
+                    title: '2019 Car Accidents',
+                    geo:{
+                        scope: 'usa',
+                        countrycolor: 'rgb(255, 255, 255)',
+                        showland: true,
+                        landcolor: 'rgb(217, 217, 217)',
+                        showlakes: true,
+                        lakecolor: 'rgb(255, 255, 255)',
+                        subunitcolor: 'rgb(255, 255, 255)',
+                    }
+                } }
+            />
+        </div> 
+    );
 }
 
 export default Map;
-
--- extramile-react/src/Pages/Maps.js
+++ extramile-react/src/Pages/Maps.js
@@ -3,7 +3,6 @@ import { MapChart } from '../Charts';
 function Maps() {
     return (
         <div>
-            <h1>Number of Accidents Per U.S. State</h1>
             <MapChart />
         </div>
     );
