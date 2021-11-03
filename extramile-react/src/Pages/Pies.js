79f2354e30239f3a7f83db5101f6ebade4c484dc
Yiu Ming Wong <dexteryiumingwong@gmail.com>
2021-11-03 12:45:38 -0700

    Added landing page for pie to display data

---
+++ extramile-react/src/Pages/Pies.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PieGraph from '../Charts/Pie'
+
+function PieChart(){
+    return (
+        <div className="content-container">
+            <PieGraph />
+        </div>
+    );
+}
+
+export default PieChart;
