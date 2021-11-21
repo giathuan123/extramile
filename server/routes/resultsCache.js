1f50f59f385aab43d5b8ff2b2870abd2c8c8f625
Gia Thuan Vu <vugiathuan123@gmail.com>
2021-11-20 23:51:03 -0800

    State increment

--- server/routes/resultsCache.js
+++ server/routes/resultsCache.js
@@ -42,12 +42,13 @@ class Result{
     Result.cache.push(this);
   } 
   recalculate(){
-    if(this.update == undefined){
-      console.log("[ERROR] update for result for url", this.url, "is undefined");
-      return;
-    }
-    for(const change of this.changeArray){
-      var answer = this.update(change);
+    var answer = null;
+    if(this.update != undefined){
+      for(const change of this.changeArray){
+        answer = this.update(change);
+      }
+    }else{
+      answer = this.computation();
     }
     this.changeArray = [];
     return answer;
@@ -69,6 +70,31 @@ class Result{
     console.log(`[INFO] Time taken for ${this.url}: ${time} ms`);
     return this.answer;
   }
+}
+function updateStateInfo([action, newData]){
+  var state = newData.State;
+  var stateInAnswerPosition = this.answer.findIndex(entry=>entry.name == state);
+  switch(action) {
+    case 'INSERT':
+      if(stateInAnswerPosition != -1){
+        this.answer[stateInAnswerPosition].accidents++;
+        var [{name, accidents}] = this.answer.splice(stateInAnswerPosition, 1);
+        var newPosition = this.answer.findIndex(ans=>ans.accidents < accidents);
+        this.answer.splice(newPosition, 0, {name: name, accidents: accidents});
+      }else{
+        var newPosition = this.answer.findIndex(ans=>ans.accidents < 1);
+        this.answer.splice(newPosition, 0, {name: city, accidents: 1});
+      }
+      break;
+    case 'DELETE':
+      this.answer[stateInAnswerPosition].accidents--;
+      var [{name, accidents}] = this.answer.splice(stateInAnswerPosition, 1);
+      var newPosition = this.answer.findIndex(ans=>ans.accidents < accidents);
+      this.answer.splice(newPosition, 0, {name: name, accidents: accidents});
+      break;
+  }
+  console.log(this.answer);
+  return this.answer;
 }
 function updateBarInfo([action, newData]){
   var city = newData.City + ", " + newData.State;
@@ -99,8 +125,8 @@ function initResults(){
   console.log("[INFO] Generating results cache");
   var barinfo = new Result('/barinfo', tenAccCities);
   barinfo.setUpdate(updateBarInfo);
-  var pieinfo = new Result('/pieinfo', SeverityChart);
-  var mostaccstateResulst = new Result('/mostaccstates', MostAccStates);
+  var mostaccstateResults = new Result('/mostaccstates', MostAccStates);
+  mostaccstateResults.setUpdate(updateStateInfo);
   var acccounties = new Result('/mostcounty', AccCounties);
   var getdailyacc = new Result('/dailystats', getDailyAccidents);
   var visgraph = new Result('/visibility', getSwarmPlotStats);
